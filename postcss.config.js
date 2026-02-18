fb6b088c7825edd78b910770e9083db888076ed8
Your Name <you@example.com>
2026-02-18 19:10:43 +0300

    fix: postcss syntax error

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,4 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
-2.  **HTML Kontrolü:** `index.html` dosyasının içinde `<div id="root"></div>` kısmının boş olduğundan ve hemen altında `<script type="module" src="/src/main.jsx"></script>` satırının doğru yolu gösterdiğinden emin ol.
+}yolu gösterdiğinden emin ol.
